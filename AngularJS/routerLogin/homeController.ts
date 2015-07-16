7f891db28902a888630fe76d44dcb93719786493
Kiyotaka Abe <k-abe@s-giken.co.jp>
2015-07-16 16:19:01 +0900

    chartを追加

---
+++ AngularJS/routerLogin/homeController.ts
@@ -0,0 +1,28 @@
+﻿module login {
+
+	export interface IHomeScope extends ng.IScope {
+		date: number;
+
+		getDate(): number;
+		logout():void ;
+	}
+
+	export class HomeController {
+		constructor(private $scope: IHomeScope, $state: ng.ui.IStateService, userService) {
+			$scope.date = 123;
+
+			$scope.getDate = () => {
+				return new Date().toIntDate();
+			}
+
+			$scope.logout = () => {
+				userService.logout();
+				$state.go("/login");
+				
+			}
+		}
+	}
+}
+
+
+
---
+++ AngularJS/routerLogin/loginController.ts
@@ -0,0 +1,33 @@
+﻿/// <reference path="../router.ts" />
+/// <reference path="../_all.ts" />
+
+module login {
+
+	export interface ILoginScope extends ng.IScope {
+		date: number;
+		getDate(): number;
+
+		login(): void;
+	}
+
+	export class LoginController {
+		constructor(private $scope: ILoginScope, $state:ng.ui.IStateService, userService) {
+			$scope.date = 123;
+			$scope.getDate = () => {
+				return new Date().toIntDate();
+			}
+
+			$scope.login = () => {
+				if (userService.login()) {
+					$state.go("/home");
+				} else {
+
+				}
+			}
+
+		}
+	}
+}
+
+
+
